17cfdbec0c44b3dd740a242983db3d73b140db34
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2026-03-28 10:32:44 -0400

    commit changes for authorization

--- tests/day03/array-of-elements.spec.js
+++ tests/day03/array-of-elements.spec.js
@@ -1,40 +1,37 @@
 import { expect, test } from "@playwright/test";
 
 test.describe("Test Group", () => {
-  test.beforeEach(async ({ page }) => {
+  let elements;
 
+  test.beforeEach(async ({ page }) => {
     await page.goto("https://the-internet-5chk.onrender.com/");
+    elements = await page.locator("//ul[@class='list-group']/li/a").all(); //will get array of locators
   });
 
-  test("Verify there are exactly 50 link elements within the <ul> tag", async ({ page }) => {
-
-let elements = page.locator("//ul[@class='list-group']/li/a").all();//will get array of locators
-
-expect((await elements).length).toBe(50);
-//expect((await elements).length).toBeGreaterThanOrEqual(20);
+  test("Verify there are exactly 50 link elements within the <ul> tag", async ({
+    page,
+  }) => {
+    expect(elements.length).toBe(50);
+    //expect(elements.length).toBeGreaterThanOrEqual(20);
   });
 
   test("Verify there each of 50 links elements within the <ul> tag visible and clickable", async ({
     page,
   }) => {
-    let elements = page.locator("//ul[@class='list-group']/li/a").all(); //will get array of locators
-    for(let e of elements){
-        await expect(e).toBeVisible();
-        expect(await e.isVisible()).toBeTruthy();
-        expect(e).toBeEnabled();
-        expect(await e.isEnabled()).toBeTruthy();
-        expect(await e.getAttribute("href")).not.toBeNull();
+    for (let e of elements) {
+      await expect(e).toBeVisible();
+      expect(await e.isVisible()).toBeTruthy();
+      expect(e).toBeEnabled();
+      expect(await e.isEnabled()).toBeTruthy();
+      expect(await e.getAttribute("href")).not.toBeNull();
     }
   });
 
-   test("Verify there each of 50 links elements within the <ul> tag has href attribute", async ({
-     page,
-   }) => {
-     let elements = page.locator("//ul[@class='list-group']/li/a").all(); //will get array of locators
-     for (let e of elements) {
-       await expect(e).toHaveAttribute("href");
-
-     }
-   });
-
+  test("Verify there each of 50 links elements within the <ul> tag has href attribute", async ({
+    page,
+  }) => {
+    for (let e of elements) {
+      await expect(e).toHaveAttribute("href");
+    }
+  });
 });
---
+++ tests/day03/web-based-authentication.spec.js
@@ -0,0 +1,24 @@
+import { expect, test } from "@playwright/test";
+
+test("Bypass authentication by embedding the credetials in the url", async ({
+  page, //https://username:password@the-internet-5chk.onrender.com/basic_auth
+}) => {
+    //not recommended for security reason since we expose username and password
+  await page.goto(
+    "https://admin:admin@the-internet-5chk.onrender.com/basic_auth",
+  );
+  await page.waitForTimeout(2000);
+});
+
+test("Encoding the credentials base64 format", async ({ page }) => {
+
+//bae64 is a way to encode the credentials in a format that is not easily readable, but it is not secure and can be easily decoded. It is not recommended to use this method for authentication.
+
+let encodedCredentials = Buffer.from("admin:admin").toString("base64"); //YWRtaW46
+//need to add to header of the request
+await page.setExtraHTTPHeaders({'Authorization': `Basic ${encodedCredentials}`});
+page.goto("https://the-internet-5chk.onrender.com/basic_auth");
+
+await page.waitForTimeout(2000);
+
+});
