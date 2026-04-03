bf32fddc9a82c8c261f458627191d7d87988f646
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2026-04-02 20:20:54 -0400

    -commit changes

---
+++ tests/reviews/review-step.spec.js
@@ -0,0 +1,71 @@
+import { test, expect } from "@playwright/test";
+import { CommonUI } from "./CommonUI";
+
+test.describe("Review Step", () => {
+  test.beforeEach(async ({ page }) => {
+    await CommonUI.login(page);
+    await CommonUI.completeStartApplicationStep(page);
+    await CommonUI.completePaymentPlanStep(page);
+  });
+
+  test("Step 1 & Step 2 steppers are green and Step 3 stepper is blue", async ({
+    page,
+  }) => {
+    let step1StepperCircle = page.locator(
+      "//div[@class='step-circle'][span='1']",
+    );
+    let step2StepperCircle = page.locator(
+      "//div[@class='step-circle'][span='2']",
+    );
+    let step3StepperCircle = page.locator(
+      "//div[@class='step-circle'][span='3']",
+    );
+
+    await expect(step1StepperCircle).toHaveCSS(
+      "background-color",
+      "rgb(172, 245, 138)",
+    );
+    await expect(step2StepperCircle).toHaveCSS(
+      "background-color",
+      "rgb(172, 245, 138)",//green
+    );
+    await expect(step3StepperCircle).toHaveCSS(
+      "background-color",
+      "rgb(1, 201, 255)",//blue
+    );
+  });
+
+  test("the payment input fields are enabled and accept card details", async ({
+    page,
+  }) => {
+    let cardNumber = "4242424242424242";
+    let expiryDate = "12/30";
+    let cvc = "123";
+    let zipCode = "12345";
+
+    await CommonUI.enterPaymentDetails(
+      page,
+      cardNumber,
+      expiryDate,
+      cvc,
+      zipCode,
+    );
+
+    let paymentFrame = page.frameLocator(
+      "(//iframe[starts-with(@name, '__privateStripeFrame')])[1]",
+    );
+    let cardNumberInput = paymentFrame.getByPlaceholder("1234 1234 1234 1234");
+    let expiryDateInput = paymentFrame.getByPlaceholder("MM / YY");
+    let cvcInput = paymentFrame.getByPlaceholder("CVC");
+    let zipCodeInput = paymentFrame.getByPlaceholder("12345");
+
+    expect(
+      (await cardNumberInput.getAttribute("value")).replace(/\s/g, ""),
+    ).toBe(cardNumber);
+    expect(
+      (await expiryDateInput.getAttribute("value")).replace(/\s/g, ""),
+    ).toBe(expiryDate);
+    await expect(cvcInput).toHaveValue(cvc);
+    await expect(zipCodeInput).toHaveValue(zipCode);
+  });
+});
