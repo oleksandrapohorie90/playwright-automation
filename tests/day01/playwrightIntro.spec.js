ec12fad7071fefe53c199aa70c44c54805975bfd
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2026-05-26 21:26:51 -0400

    commit chnages

--- tests/day01/playwrightIntro.spec.js
+++ tests/day01/playwrightIntro.spec.js
@@ -1,6 +1,8 @@
 //const {test} = require("playwright/test");
 import {test} from "@playwright/test";
 
+//{} to pass a set of fixtures - page fixture we need for every single autmation test, so we can destructure it and use it in our test
+
 test("Simple google test", async ({page}) => {
     //navigate to the specific url, method returns promise, so we need to await it
     await
