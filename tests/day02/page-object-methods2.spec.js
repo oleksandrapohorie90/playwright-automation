14ee6e8bbdff875924df863d53d03a1386f3792b
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2026-05-27 22:32:59 -0400

    commit changes

---
+++ tests/day02/page-object-methods2.spec.js
@@ -0,0 +1,31 @@
+import { test } from "@playwright/test";
+
+test("Get the title of the page", async ({ page }) => {
+
+    await page.goto("https://the-internet-5chk.onrender.com/");
+
+    let actualTitle = await page.title();
+    console.log(actualTitle);
+    
+});
+
+test("Get the URL of the page", async ({ page }) => {
+
+    await page.goto("https://the-internet-5chk.onrender.com/");
+
+    let actualURL = await page.url();
+    console.log(actualURL);
+});
+
+test("Set the window size", async ({ page }) => {
+
+  await page.goto("https://the-internet-5chk.onrender.com/");
+  
+  await page.setViewportSize({ width: 800, height: 600 });
+
+  let actualURL = await page.url();
+  console.log(actualURL);
+});
+
+
+//================================
