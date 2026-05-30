876014bbc242ec886365b6845db4ebd3f693740c
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2026-05-30 19:01:28 -0400

    commit common methods of locator obj

---
+++ tests/day02/test-group-practice2.spec.js
@@ -0,0 +1,12 @@
+import { test } from '@playwright/test';
+
+test.describe('', () => {
+  test('A', async ({ page }) => {
+  });
+
+  test('B', async ({ page }) => {
+  });
+
+  test('C', async ({ page }) => {
+  });
+});
